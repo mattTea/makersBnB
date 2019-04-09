415ef11187a1d6e823afb320bc0c76ccd63b3260
Evelyn <evelyn@ipsoft.es>
2019-04-09 10:21:57 +0100

    Refactored to only have one submit button

--- src/SpaceInterface.js
+++ src/SpaceInterface.js
@@ -5,19 +5,12 @@ $(document).ready(function() {
   $('#name').text(space.name);
   $('#description').text(space.description);
 
-  $('#submit-name').click(function() {
+  $('#submit-new-space').click(function() {
     space.addName($('#space-name').val());
     $('#name').text(space.name);
-  });
-
-  $('#submit-description').click(function() {
     space.addDescription($('#space-description').val());
     $('#description').text(space.description);
-  });
-
-  $('#submit-price').click(function() {
     space.addPrice($('#space-price-per-night').val());
     $('#price-per-night').text(`£${space.price}`);
-  }); 
-
+  });
 })
