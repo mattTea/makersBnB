092161afcd34d5d45d5463e20ef5c5a35fef5da6
Amy Jordan <amy_jordan@live.co.uk>
2019-04-10 09:47:50 +0100

    Uses DatabaseConnection in User

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,3 @@
-require 'pg'
-
 class User
   attr_reader :name, :username, :email, :password
 
@@ -12,13 +10,7 @@ class User
   end
 
   def self.create(name:, username:, email:, password:)
-
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'pinkbnb_test')
-    else
-      connection = PG.connect(dbname: 'pinkbnb')
-    end
-    result = connection.exec(
+    result = DatabaseConnection.query(
       "INSERT INTO users (name, username, email, password)
       VALUES ('#{name}', '#{username}', '#{email}', '#{password}')
       RETURNING id, name, username, email, password;"
@@ -33,12 +25,7 @@ class User
   end
 
   def self.all
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'pinkbnb_test')
-    else
-      connection = PG.connect(dbname: 'pinkbnb')
-    end
-    result = connection.exec("SELECT * FROM users;")
+    result = DatabaseConnection.query("SELECT * FROM users;")
     result.map do |user|
       User.new(
         id: user['id'],
