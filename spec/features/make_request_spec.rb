2cbe2b97db79d1771f1e11b94e2a2c1cfcf3f304
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-11 11:20:54 +0100

    User story 7: Guest requests space (Part 1 - sets up requests db, creates request instance)
    As a guest,
    So that I can go on holiday
    I want to request a space for a night

---
+++ spec/features/make_request_spec.rb
@@ -0,0 +1,41 @@
+xfeature "Make request" do
+  scenario "user request a space for a date" do
+    # create a host user
+    host_user = User.create(
+      name: "George",
+      username: "georgie",
+      email: "georgie@com",
+      password: "password123"
+    )
+    visit "/"
+    fill_in("login_username", with: "georgie")
+    fill_in("login_password", with: "password123")
+    click_button("Login")
+    
+    # create a space
+    click_button("Add space")
+    fill_in('name', with: 'Hilton')
+    fill_in('description', with: 'Fancy hotel')
+    fill_in('price_per_night', with: 100)
+    click_button('Add')
+
+    click_button('Logout')
+    
+    # create guest user
+    guest_user = User.create(
+      name: "Evelyn",
+      username: "evelyn",
+      email: "evelyn@com",
+      password: "password1"
+    )
+    visit "/"
+    fill_in("login_username", with: "georgie")
+    fill_in("login_password", with: "password123")
+    click_button("Login")
+    # make request
+    click_button('Request')
+
+    # expect confirmation of request
+    expect(page).to have_content "You have requested Hilton"
+  end
+end
