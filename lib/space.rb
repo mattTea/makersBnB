679fe514e61d4aad59d39f577520bdf3e9446e87
Amy Jordan <amy_jordan@live.co.uk>
2019-04-11 11:04:42 +0100

    Shows spaces' available dates
    Assigned available date attributes to space table.
    Updated feature and unit tests.
    Updates logic and view.

--- lib/space.rb
+++ lib/space.rb
@@ -1,26 +1,30 @@
 class Space
 
-  attr_reader :id, :name, :description, :price, :user_id
+  attr_reader :id, :name, :description, :price, :start_date, :end_date, :user_id
 
-  def initialize(id:, name:, description:, price:, user_id:)
+  def initialize(id:, name:, description:, price:, start_date:, end_date:, user_id:)
     @id = id
     @name = name
     @description = description
     @price = price
+    @start_date = start_date
+    @end_date = end_date
     @user_id = user_id
   end
 
-  def self.create(name:, description:, price:, user_id:)
+  def self.create(name:, description:, price:, start_date:, end_date:, user_id:)
     result = DatabaseConnection.query(
-      "INSERT INTO spaces (name, description, price_per_night, user_id)
-      VALUES ('#{name}', '#{description}', '#{price}', '#{user_id}')
-      RETURNING id, name, description, price_per_night, user_id;"
+      "INSERT INTO spaces (name, description, price_per_night, user_id, start_date, end_date)
+      VALUES ('#{name}', '#{description}', '#{price}', '#{user_id}', '#{start_date}', '#{end_date}')
+      RETURNING id, name, description, price_per_night, user_id, start_date, end_date;"
     )
     Space.new(
       id: result[0]['id'],
       name: result[0]['name'],
       description: result[0]['description'],
       price: result[0]['price_per_night'],
+      start_date: result[0]['start_date'],
+      end_date: result[0]['end_date'],
       user_id: result[0]['user_id']
     )
   end
@@ -33,6 +37,8 @@ class Space
         name: space['name'],
         description: space['description'],
         price: space['price_per_night'],
+        start_date: space['start_date'],
+        end_date: space['end_date'],
         user_id: space['user_id']
       )
     end
--- spec/features/create_new_space_spec.rb
+++ spec/features/create_new_space_spec.rb
@@ -14,10 +14,14 @@ feature 'create new space' do
     fill_in('name', with: 'Hilton')
     fill_in('description', with: 'Fancy hotel')
     fill_in('price_per_night', with: 100)
+    fill_in('start_date', with: '2019-04-04')
+    fill_in('end_date', with: '2019-04-10')
     click_button('Add')
     expect(page).to have_content('Name: Hilton')
     expect(page).to have_content('Description: Fancy hotel')
     expect(page).to have_content('Price per night: $100.00')
+    expect(page).to have_content('Available from: 4 April 2019')
+    expect(page).to have_content('Available until: 10 April 2019')
   end
 
   scenario 'link user id to space when creating new space' do
@@ -35,6 +39,8 @@ feature 'create new space' do
     fill_in('name', with: 'Hilton')
     fill_in('description', with: 'Fancy hotel')
     fill_in('price_per_night', with: 100)
+    fill_in('start_date', with: '2019-04-04')
+    fill_in('end_date', with: '2019-04-10')
     click_button('Add')
     expect(page).to have_content 'Host: georgie'
   end
