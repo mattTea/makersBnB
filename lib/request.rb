72446d75c9fb9f31bb60aa39e6455420fc7b476d
Amy Jordan <amy_jordan@live.co.uk>
2019-04-12 10:03:31 +0100

    Retrieves all requests by host user id

--- lib/request.rb
+++ lib/request.rb
@@ -1,6 +1,6 @@
 class Request
   attr_reader :user_id, :space_id, :date
-  
+
   def initialize(id:, user_id:, space_id:, date:)
     @user_id = user_id
     @space_id = space_id
@@ -19,5 +19,19 @@ class Request
       space_id: result[0]['space_id'],
       date: result[0]['date']
     )
+  end
+
+  def self.all_by_user(user_id:)
+    result = DatabaseConnection.query("SELECT requests.id, requests.date, requests.space_id, requests.user_id FROM requests
+      JOIN spaces ON (spaces.id=space_id) JOIN users
+      ON (spaces.user_id=users.id) WHERE (users.id='#{user_id}');")
+    result.map do |request|
+        Request.new(
+        id: request['id'],
+        user_id: request['user_id'],
+        space_id: request['space_id'],
+        date: request['date']
+      )
+    end
   end
 end
--- spec/units/request_spec.rb
+++ spec/units/request_spec.rb
@@ -19,8 +19,34 @@ describe "Request" do
         email: "evelyn@com",
         password: "password1"
       )
-      request = Request.create(user_id: guest_user.id, space_id: space.id, date: "11th April")
+      request = Request.create(user_id: guest_user.id, space_id: space.id, date: "2019-04-11")
       expect(request).to be_a Request
+    end
+  end
+
+  describe ".all_by_user" do
+    it "returns all requests for a specific user" do
+      host_user = User.create(
+        name: "George",
+        username: "georgie",
+        email: "georgie@com",
+        password: "password123"
+      )
+
+      space = Space.create(name: "Hilton", description: "Posh hotel", price: 100, user_id: host_user.id, start_date: '2019-04-10', end_date: '2019-04-17')
+
+      guest_user = User.create(
+        name: "Evelyn",
+        username: "evelyn",
+        email: "evelyn@com",
+        password: "password1"
+      )
+      Request.create(user_id: guest_user.id, space_id: space.id, date: "2019-04-12")
+      Request.create(user_id: guest_user.id, space_id: space.id, date: "2019-04-13")
+
+      requests = Request.all_by_user(user_id: host_user.id)
+      expect(requests.length).to eq 2
+      expect(requests.first.space_id).to eq space.id
     end
   end
 end
