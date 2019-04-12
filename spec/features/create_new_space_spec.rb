b13ffa37e558577b43a0a8024bdb0c3b08d8eeef
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-12 11:59:39 +0100

    Adjusted feature tests due to new front end format

--- spec/features/create_new_space_spec.rb
+++ spec/features/create_new_space_spec.rb
@@ -17,11 +17,11 @@ feature 'create new space' do
     fill_in('start_date', with: '2019-04-04')
     fill_in('end_date', with: '2019-04-10')
     click_button('Add')
-    expect(page).to have_content('Name: Hilton')
-    expect(page).to have_content('Description: Fancy hotel')
-    expect(page).to have_content('Price per night: $100.00')
-    expect(page).to have_content('Available from: 4 April 2019')
-    expect(page).to have_content('Available until: 10 April 2019')
+    expect(page).to have_content('Hilton')
+    expect(page).to have_content('Fancy hotel')
+    expect(page).to have_content('$100.00')
+    expect(page).to have_content('4 April 2019')
+    expect(page).to have_content('10 April 2019')
   end
 
   scenario 'link user id to space when creating new space' do
@@ -42,6 +42,7 @@ feature 'create new space' do
     fill_in('start_date', with: '2019-04-04')
     fill_in('end_date', with: '2019-04-10')
     click_button('Add')
-    expect(page).to have_content 'Host: georgie'
+    expect(page).to have_content 'Host:'
+    expect(page).to have_content 'georgie'
   end
 end
